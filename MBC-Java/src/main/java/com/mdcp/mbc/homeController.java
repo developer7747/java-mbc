cbd991d576610e12d4ddfdb37da878d7fa84e020
poojarandive <poojarandive741@gmail.com>
2017-03-13 19:19:49 +0530

    SEcond commit

--- MBC-Java/src/main/java/com/mdcp/mbc/homeController.java
+++ MBC-Java/src/main/java/com/mdcp/mbc/homeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class homeController {
+	
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home()
